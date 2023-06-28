5fec8efd4614c69e0725bf0a4f18933f2300c2c3
ParkJuHan94 <1004sunye@naver.com>
2023-06-28 19:43:36 +0900

    start

---
+++ LabStore/src/main/java/com/project/LabStore/LabStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.LabStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabStoreApplication.class, args);
+	}
+
+}
